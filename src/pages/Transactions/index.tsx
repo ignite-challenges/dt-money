3a66006f2c2e7ad018c31ba15c759bedf629ec22
Tiago Oliveira <tiago.-oliveira@hotmail.com>
2023-03-18 17:16:44 -0300

    chore: search input component

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,10 +1,39 @@
 import { Header } from "../../components/Header";
-import { TransactionsContainer } from "./styles";
+import { Summary } from "../../components/Summary";
+import { SearchForm } from "./SearchForm";
+import { PriceHighlight, TransactionsContainer, TransactionsTable } from "./styles";
 
 export function Transactions() {
   return (
-    <TransactionsContainer>
+    <div>
       <Header />
-    </TransactionsContainer>
+      <Summary />
+
+      <TransactionsContainer>
+        <SearchForm />
+        <TransactionsTable>
+          <tr>
+            <td width={'50%'}>Desenvolvimento de site</td>
+            <td>
+              <PriceHighlight variant="income">
+                R$ 12.000,00
+              </PriceHighlight>
+            </td>
+            <td>Venda</td>
+            <td>13/04/2022</td>
+          </tr>
+          <tr>
+            <td width={'50%'}>Hamburguer</td>
+            <td>
+              <PriceHighlight variant="outcome">
+                -R$ 12,00
+              </PriceHighlight>
+            </td>
+            <td>Venda</td>
+            <td>13/04/2022</td>
+          </tr>
+        </TransactionsTable>
+      </TransactionsContainer>
+    </div>
   );
 };
